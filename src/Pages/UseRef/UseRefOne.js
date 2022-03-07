9a0332f3738077a882f46b7281c08a691a268421
DevIdol <devidol.mm@gmail.com>
2022-03-07 19:45:24 +0630

    useRef

--- src/Pages/UseRef/UseRefOne.js
+++ src/Pages/UseRef/UseRefOne.js
@@ -4,19 +4,28 @@ const UseRefOne = () => {
   const [inputValue, setInputValue] = useState("");
 
   const count = useRef(0);
+  const preInputValue = useRef("");
+  const inputElement = useRef();
+
+  const focusInput = ()=> inputElement.current.focus()
 
   useEffect(() => {
-    count.current = count.current + 1;
-  });
+    count.current++;
+    preInputValue.current = inputValue;
+  }, [inputValue]);
 
   return (
-    <div style={{padding: "30px"}}>
+    <div style={{ padding: "30px" }}>
       <input
         type="text"
+        ref={inputElement}
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
+      <button onClick={focusInput} >Foucs</button>
       <h2>Value Count: {count.current}</h2>
+      <h2>Input Value: {inputValue}</h2>
+      <h2>Previous Value: {preInputValue.current}</h2>
     </div>
   );
 };
