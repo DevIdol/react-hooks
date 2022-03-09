d99eb3c4d901cad4434bf56c749d549b52b029bb
DevIdol <devidol.mm@gmail.com>
2022-03-09 12:05:35 +0630

    useRef

--- src/Pages/UseRef/UseRefTwo.js
+++ src/Pages/UseRef/UseRefTwo.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React, { useRef, useState } from "react";
 
 const UseRefTwo = () => {
-    return (
-        <div>
-          <h1>  UseRef Two</h1>
-        </div>
-    );
+  const [second, setSecond] = useState(0);
+
+  const render = useRef(0);
+  const timerId = useRef();
+  console.log(timerId.current);
+  const startTimer = () => {
+    timerId.current = setInterval(() => {
+      render.current++;
+      // console.log(render.current)
+      setSecond((pre) => pre + 1);
+    }, 1000);
+
+  };
+
+  const resetTimer = () => {
+    stopTimer();
+    if (second) {
+      // render.current++;
+      setSecond(0);
+    }
+  };
+
+  const stopTimer = () => {
+    clearInterval(timerId.current);
+  };
+  return (
+    <div style={{ padding: "30px" }}>
+      <h2>Timer: {second}</h2>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer} style={{ margin: "10px" }}>
+        Stop
+      </button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
 };
 
 export default UseRefTwo;
