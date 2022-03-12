766c88f3c6ddffc5b837a5ec028e3df629777f85
DevIdol <devidol.mm@gmail.com>
2022-03-12 13:52:10 +0630

    useCallback

--- src/Pages/UseCallback/UseCallbackTwo.js
+++ src/Pages/UseCallback/UseCallbackTwo.js
@@ -4,13 +4,16 @@ import List from "./List";
 const UseCallbackTwo = () => {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
-  const getItems = useCallback((arg) => {
-    return [number + arg, number + 1 + arg, number + 2 + arg];
-  },[number]);
+  const getItems = useCallback(
+    (arg) => {
+      return [number + arg, number + 1 + arg, number + 2 + arg];
+    },
+    [number]
+  );
   const theme = {
     backgroundColor: dark ? "#333" : "#fff",
     color: dark ? "#fff" : "#333",
-    padding: "30px"
+    padding: "30px",
   };
   return (
     <div style={theme}>
@@ -20,7 +23,12 @@ const UseCallbackTwo = () => {
         onChange={(e) => setNumber(parseInt(e.target.value))}
       />
       <br />
-      <button style={{padding: "4px 8px"}} onClick={() => setDark((pre) => !pre)}>ToggleTheme</button>
+      <button
+        style={{ padding: "4px 8px" }}
+        onClick={() => setDark((pre) => !pre)}
+      >
+        ToggleTheme
+      </button>
       <List getItems={getItems} />
     </div>
   );
