190c0681bc3d636cdbe455b7f36a7f1aa49310d4
DevIdol <devidol.mm@gmail.com>
2022-03-12 16:27:57 +0630

    Custom Hook

--- src/Pages/CustomHook/CustomHookOne.js
+++ src/Pages/CustomHook/CustomHookOne.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
 
 const CustomHookOne = () => {
-    return (
-        <div style={{padding: "30px"}}>
-            <h1>CustomHook One</h1>
-        </div>
-    );
+  const [data] = useFetch("https://jsonplaceholder.typicode.com/todos");
+  console.log(data);
+
+  return (
+    <div style={{ padding: "30px" }}>
+      <ol>
+      {data && data.map((item) => <li key={item.id}>{JSON.stringify(item)}</li>)}
+      </ol>
+    </div>
+  );
+};
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  useEffect(() => {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [url]);
+
+  return [data];
 };
 
 export default CustomHookOne;
